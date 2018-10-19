4202af92831628c414f87a222528e83b3c445f3f
spinaBelaya <serjd2112@gmail.com>
2018-10-19 10:24:43 +0300

    Добавлен шаблон для шифра Цезаря

---
+++ homework01/caesar.py
@@ -0,0 +1,28 @@
+def encrypt_caesar(plaintext: str) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    return ciphertext
+
+def decrypt_caesar(ciphertext):
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    return plaintext
+
